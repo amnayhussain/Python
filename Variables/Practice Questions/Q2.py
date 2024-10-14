5bb490a76b09e92feb18e4cab3d76f9221c93604
Amna <amnaahussain.a@gmail.com>
2024-10-14 12:41:38 +0500

    Add a Practice Questions and a README file to show you How to declare a Variable name

---
+++ Variables/Practice Questions/Q2.py
@@ -0,0 +1,8 @@
+# Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
+
+# Assign a message to a variable
+message = "This is the First message"
+print(message)
+# Change the value of the variable
+message = "This is the new meaasge"
+print(message)
